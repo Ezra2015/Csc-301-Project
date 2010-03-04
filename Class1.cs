48af9aa21643a9937e3043c31c8fd670175aecbb
unknown <E@.(none)>
2010-03-05 03:50:41 +0800

    csc301 Project created...

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParcelDeliverySystem
+{
+    class Class1
+    {
+        public static void main(string[] args){
+            System.Console.WriteLine("hhihihi");
+        }
+    }
+}
